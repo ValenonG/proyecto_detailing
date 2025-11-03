c4b9426dedc8a44767cd9a1516a11f8455834c55
ValenonG <valenobgarcia@gmail.com>
2025-11-02 22:48:43 -0300

    [ADD] initial config dotenv and cors

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-import express from 'express';
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
-const app = express();
+dotenv.config()
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Server is ON');
-});
+  res.send('Server is ON')
+})
 
-app.listen(3000, () => {
-  console.log(`Server is running on port 3000`);
-});
+app.listen(PORT, () => {
+  console.log(`Server is running on port 3000`)
+})
