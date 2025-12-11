8d9035f464fa8f01e1b8feeb24ecacadafede196
Leonardo La Rosa <leonardolarosa1190@gmail.com>
2025-12-11 20:40:44 -0300

    Se agrego la ruta del middleware y se completo el CRUD

--- src/routes/persona/index.ts
+++ src/routes/persona/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 
 import controllers from './controllers';
+import validationMiddleware from '../../middlewares/validator';
+import { createPersonaValidationSchema } from './validations';
 
 const router = express.Router();
 
-router.get('/', controllers.createPersona);
+router.post('/', validationMiddleware(createPersonaValidationSchema), controllers.createPersona);
+router.post('/', controllers.createPersona);
 
 export default router;
