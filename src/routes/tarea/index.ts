949d72110d3a9939f8528c7e0d7242b0b61e597c
JulianDeco <juliandecoppet@gmail.com>
2025-12-14 12:47:01 -0300

    Corregir rutas de tareas
    - Cambiar GET / a getAllTareas en lugar de createTarea
    - Mover createTarea a POST /
    - Hacer pública la ruta GET / para mostrar servicios sin autenticación
    - Hacer pública la ruta GET /:id

--- src/routes/tarea/index.ts
+++ src/routes/tarea/index.ts
@@ -5,9 +5,9 @@ import { authenticateFirebase } from '../../middlewares/authenticateFirebase';
 
 const router = express.Router();
 
-router.get('/', authenticateFirebase, controllers.createTarea);
-router.get('/all', controllers.getAllTareas);
-router.get('/:id', authenticateFirebase, controllers.getTareaById);
+router.get('/', controllers.getAllTareas);
+router.post('/', authenticateFirebase, controllers.createTarea);
+router.get('/:id', controllers.getTareaById);
 router.patch('/:id', authenticateFirebase, controllers.updateTarea);
 router.delete('/hard/:id', authenticateFirebase, controllers.hardDeleteTarea);
 router.patch('/soft/:id', authenticateFirebase, controllers.softDeleteTarea);
